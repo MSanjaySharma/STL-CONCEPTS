e7874c7bb5b14d59a89fe0d58598b1945806ac6e
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-09-11 17:02:30 +0530

    Updated Vector

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+    vector<int> A = {56, 7, 92, 6, 9};
+
+    cout << A[4] << endl; //9
+
+    sort(A.begin(), A.end()); // O(nlog(n))
+    /* 6, 7, 9, 56, 92 */
+    /* sort mutates the vector */
+
+    cout << A[4] << endl; //92
+
+    /* Binary Search - O(logN) */
+    bool present = binary_search(A.begin(), A.end(), 7);
+
+    std::cout << std::boolalpha << present << endl; // 1 --- true
+
+    present = binary_search(A.begin(), A.end(), 3);
+
+    std::cout << std::boolalpha << present << endl; //0 --- false
 }
